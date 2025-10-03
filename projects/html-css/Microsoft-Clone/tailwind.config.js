a01a4649239425ca9012ee8053960814278b1b81
Mani <77221000+Eternity0207@users.noreply.github.com>
2025-10-03 06:12:02 +0530

    Added Microsoft Clone

---
+++ projects/html-css/Microsoft-Clone/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      boxShadow: {
+        glow: '0 0 10px rgba(0, 0, 0, 0.2), 0 0 20px rgba(49, 49, 49, 0.05)',
+      },
+    },
+  },
+  plugins: [],
+}
